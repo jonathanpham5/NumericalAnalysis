fb059bbfe02a087b59147d007675d319dd6ed159
Jonathan <jtpham@shavanosystems.com>
2017-04-06 11:19:30 -0600

    updated gui

--- gui.py
+++ gui.py
@@ -1,18 +1,37 @@
+import time
 from Tkinter import *
+from ttk import *
+
 class App(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
+    def __init__(self):
+        self.root = Tk()
+        self.root.s = Style()
+        self.root.s.theme_use("clam")
+        self.root.title("Numerical Analysis - Project 2")
+        
+        Frame.__init__(self, self.root)
+        self.createWidgets()
         self.pack()
 
+    def wait(self):
+        while not w.can_make_request():
+            time.sleep(1)
 
+    def createWidgets(self):
+        self.grid()
+
+        # Application Widgets
+        self.testLabel = Label(self, text = "Numerical Analysis")
+
+        # Grid placement of Widgets
+        self.testLabel.grid(row = 0, column = 0)
+    
+    def start(self):
+        self.root.mainloop()
+
+        
 # create the application
-myapp = App()
-
-#
-# here are method calls to the window manager class
-#
-myapp.master.title("My Do-Nothing Application")
-myapp.master.maxsize(1000, 400)
+gui = App()
 
 # start the program
-myapp.mainloop()
+gui.start()
