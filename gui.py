fd30cb931b463d7ea314ceb899a89ce783f5c181
Jonathan <jtpham@shavanosystems.com>
2017-04-06 14:50:31 -0600

    GUI Template Updated

--- gui.py
+++ gui.py
@@ -33,7 +33,7 @@ class App(Frame):
     def __init__(self):
         self.root = Tk()
         self.root.s = Style()
-        self.root.geometry("400x300")
+        self.root.geometry("420x300")
         self.root.resizable(0,0)
         self.root.s.theme_use("clam")
         self.root.title("Numerical Analysis - Project 2")
@@ -53,32 +53,32 @@ class App(Frame):
                                    padx = 5, pady = 5, ipadx = 5, ipady = 5)
         self.interpLabelframe.pack(fill = 'both', expand = 'yes')
         self.innerInFrame = Frame(self.interpLabelframe)
-        self.innerInFrame.grid(padx = 70)
+        self.innerInFrame.grid(padx=1)
         
         # Interpolation Frame Buttons
-        self.chebyButton = Button(self.innerInFrame, text = "Chebyshev", command = self.chebyWindow)
-        self.splinesButton = Button(self.innerInFrame, text = "Splines (cubic)")
-        self.bezierButton = Button(self.innerInFrame, text = "Bezier")
+        self.chebyButton = Button(self.innerInFrame, text = "Chebyshev", width=20, command = self.chebyWindow)
+        self.splinesButton = Button(self.innerInFrame, text = "Splines (cubic)", width=20, command = self.splinesWindow)
+        self.bezierButton = Button(self.innerInFrame, text = "Bezier", width=21, command = self.bezierWindow)
         self.chebyButton.grid(row = 0, column = 0)
         self.splinesButton.grid(row = 0, column = 1)
         self.bezierButton.grid(row = 0, column = 2)
 
         # Least Squares Frame
         self.lsLabelframe = LabelFrame(self.root, text = "Least Squares", labelanchor = N)
-        self.lsLabelframe.grid(row = 2, columnspan = 7, sticky = 'WE', \
+        self.lsLabelframe.grid(row = 2, sticky = 'WE', \
                                padx = 5, pady = 5, ipadx = 5, ipady = 5)
         self.lsLabelframe.pack(fill = 'both', expand = 'yes')
-        self.innerLsFrame = Frame(self.lsLabelframe )
+        self.innerLsFrame = Frame(self.lsLabelframe)
         self.innerLsFrame.grid(padx = 118)
         
         # Least Squares Frame Buttons
-        self.linearBtn = Button(self.lsLabelframe, text = "Linear")
-        self.nonLinBtn = Button(self.lsLabelframe, text = "Nonlinear")
-        self.linearBtn.grid(row = 0, column = 0)
-        self.nonLinBtn.grid(row = 0, column = 2)
+        self.linearBtn = Button(self.lsLabelframe, text = "Linear", width=27)
+        self.nonLinBtn = Button(self.lsLabelframe, text = "Nonlinear", width=27)
+        self.linearBtn.grid(row = 0, column = 0, sticky = W)
+        self.nonLinBtn.grid(row = 0, column = 1)
 
         # Differentiation and Integration Frame
-        self.diffAndInt = LabelFrame(self.root, text = "Differentiation and Integration", labelanchor = N)
+        self.diffAndInt = Frame(self.root)#, text = "Differentiation and Integration", labelanchor = N)
         self.diffAndInt.grid(row = 0, columnspan = 7, sticky = 'WE', \
                                    padx = 5, pady = 5, ipadx = 10, ipady = 5)
         self.diffAndInt.pack(fill = 'both', expand = 'yes')
@@ -93,12 +93,12 @@ class App(Frame):
         
         # Differentiation and Integration Buttons
         self.differenceBtn = Button(self.diffFrame, text = "Difference Methods")
-        self.extrapBtn = Button(self.diffFrame, text = "Extrapolation")
+        self.extrapBtn = Button(self.diffFrame, text = "Extrapolation", width=20)
         self.autoDiffBtn = Button(self.diffFrame, text = "Automatic Differentiation")
         
-        self.newtonCodesBtn = Button(self.intFrame, text = "Newton-Codes")
-        self.rombergBtn = Button(self.intFrame, text = "Romberg")
-        self.adaptBtn = Button(self.intFrame, text = "Adaptive")
+        self.newtonCodesBtn = Button(self.intFrame, text = "Newton-Codes", width=20)
+        self.rombergBtn = Button(self.intFrame, text = "Romberg", width=20)
+        self.adaptBtn = Button(self.intFrame, text = "Adaptive", width=20)
 
         self.differenceBtn.grid(row = 0, column = 0)
         self.extrapBtn.grid(row = 0, column = 1)
@@ -119,18 +119,27 @@ class App(Frame):
     def chebyWindow(self):
         # create window
         self.cheby = Toplevel()
-        quitBtn = Button(self.cheby, text = "Quit", command = self.quitChildWindow)
+        quitBtn = Button(self.cheby, text = "Submit")
         quitBtn.pack()
 
+    def splinesWindow(self):
+        # create window
+        self.splines = Toplevel()
+        quitBtn = Button(self.splines, text = "Submit")
+        quitBtn.pack()
+
+    def bezierWindow(self):
+        # create window
+        self.bezier = Toplevel()
+        quitBtn = Button(self.bezier, text = "Submit")
+        quitBtn.pack()
+        
     def start(self):
         self.root.mainloop()
 
     def quitApp(self):
         self.root.destroy()
 
-    def quitChildWindow(self):
-        self.cheby.destroy()
-        self.root.deiconify()
         
 # create the application
 gui = App()
